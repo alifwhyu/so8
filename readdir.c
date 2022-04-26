ec08bc134e66b59a1ea7718aae0d8a140ca6e82f
alifwhyu <101910498+alifwhyu@users.noreply.github.com>
2022-04-27 05:05:26 +0700

    Update readdir.c

--- readdir.c
+++ readdir.c
@@ -1,19 +1,15 @@
-#include <dirent.h>
 #include <stdio.h>
 #include <stdlib.h>
-int main(int argc, char **argv)
+int main()
 {
- if (argc == 1)
- {
- printf("Usage: %s [directory]\n", *argv);
- exit(0);
- }
- struct dirent *dp;
- DIR *dirp = opendir(argv[1]);
- while ((dp = readdir(dirp)) != NULL)
- {
- printf("%s %lu\n", dp-> d_name, (unsigned long)dp-> d_ino
- }
- closedir(dirp);
- return 0;
-};
+    FILE *fh;
+    fh = fopen("Praktikum_IX.txt", "w");
+    if (fh == NULL)
+    {
+        puts("Can't open that file!");
+        exit(1);
+    }
+    fprintf(fh, "Look what I made!n");
+    fclose(fh);
+    return (0);
+}
